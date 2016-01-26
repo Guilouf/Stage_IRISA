144687669c483a773a1938a31d0bdb4c0035fed4
guilouf <guigore@wanadoo.fr>
2016-01-26 17:20:57 +0100

    orm relation

--- bddbis.py
+++ bddbis.py
@@ -2,7 +2,7 @@
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationships
 
 eng = create_engine('sqlite:///testBis.balec')
 
@@ -10,32 +10,37 @@ Base = declarative_base()
 
 class Accessions(Base):  # le truc (Base) c'est l'héritage
 
-    __tablename__ = "Accessions"
+    __tablename__ = "Accessions_tab"
 
     Id = Column(Integer, primary_key=True)
     Access = Column(String)
 
+    #hasRefSeq = relationships('Friends', primaryjoin=lambda: id == EC_numbers.Id_ec)
+    hasRefSeq = relationships()
+
 
 class EC_numbers(Base):
 
-    __tablename__ = "EC_numbers"
+    __tablename__ = "EC_numbers_tab"
 
     Id_ec = Column(Integer, primary_key=True)
     num_ec = column(String)
 
-class RefSeq: # manque les clés primaires ici
+"""
+class RefSeq(Base):  # bug avec le base ici
 
-    __tablename__ = "EC_numbers"
+    __tablename__ = "RefSeq_tab"
 
-    col_accession = Column(String)
-    col_ec = Column(String)
+    col_accession = Column(String, ForeignKey(Accessions.Id))
+    col_ec = Column(String, ForeignKey(EC_numbers.Id_ec))
 
 class Primaire:
 
-    __tablename__ = "EC_numbers"
+    __tablename__ = "Primaire_tab"
 
     col_accession = Column(String)
     col_ec = Column(String)
+"""
 
 Base.metadata.bind = eng
 Base.metadata.create_all()
