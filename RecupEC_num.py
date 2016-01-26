2ce79e78109dd912334dde1a93fb8f0a6040871c
Guilouf <Guilouf@users.noreply.github.com>
2016-01-26 18:32:47 +0100

    commit a suppr

--- RecupEC_num.py
+++ RecupEC_num.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from Bio import Entrez, SeqIO
+from io import StringIO
 from Bio.SeqRecord import SeqRecord
 from BaseDeDonnees import BaseDDCl
 import copy
@@ -27,17 +28,20 @@ class Recup_EC :
         Entrez.email = "ouiouioui@wanadoo.fr"
 
     def telecharge(self, accession):
-
+        """
+        bon apparement le read serait pour le xml..
+        :param accession:
+        :return:
+        """
         handle = Entrez.efetch(db="nucleotide", id=accession, rettype="gbwithparts", retmode="txt")
 
-        gbk = handle.read()
+        gbk =  handle.read() #readline marche aussi, mais renvoit juste une ligne
+        gbk = StringIO(gbk)
+        #print(gbk.read()) # bon ca marche parfaitement bien, sauf si on fait le read en dehors de la fct..
+        #GenBankScanner.parse(handle)
+        handle.close()
+        return gbk   # je l'ai passé en handle tout court
 
-        #self.detection()
-
-        """
-        with open("exemple/gbwithparts_Refseq_Master", 'w') as fichier:  # ok ca marche faut juste créer la directory
-            fichier.write(textt)
-        """
 
     def detection(self, gbk=None):
         """
@@ -110,16 +114,40 @@ class Recup_EC :
 
 if __name__ == "__main__":
     recu = Recup_EC()
+    """
     with open("exemple/gbwithparts_Refseq_Complet", 'r') as fichierGBKcomplet:
         fichierParseComplet = SeqIO.parse(fichierGBKcomplet, 'genbank')
         #recu.recup_ec(fichierParseComplet)
+    """
+    #test à partir du handle..
+    handleRead = recu.telecharge("NZ_AZSI00000000")
+    fichierParseMaster = SeqIO.parse(handleRead, 'genbank')
+    #print(handleRead.read())
+    print("putain que ca m'enerve..")
+
+    putainhandle = handleRead
+    print(putainhandle)
+    #print(fichierParseMaster)
+
+    for i in putainhandle: # le generateur bug avec le handle...
+        print(i)
+
+
+    for machin in SeqIO.parse(handleRead, 'genbank'):
+        print(machin)
+
+
+
 
     with open("exemple/gbwithparts_Refseq_Master", 'r') as fichierGBKmaster:
         fichierParseMaster = SeqIO.parse(fichierGBKmaster, 'genbank')
+        #fichierParseMaster = recu.telecharge("NZ_AZSI00000000")
+        #fichierParseMaster = SeqIO.parse(recu.telecharge("NZ_AZSI00000000"), 'genbank')
         #recu.recup_master_access(fichierParseMaster)
 
     #gbwithparts_Refseq_Master
     #gbwithparts_Refseq_Complet
+    """
     with open("exemple/gbwithparts_Refseq_Master", 'r') as test:
         test_parse, gbk_gener = tee( SeqIO.parse(test, 'genbank') ) # ok, tee de itertools permet de creer plusieur gener
         #gbk_gener = SeqIO.parse(test, 'genbank')
@@ -133,7 +161,7 @@ if __name__ == "__main__":
             for access in recu.recup_master_access(gbk_gener):  # faire une boucle de telechargement ici
                 pass # mm en pass ca active le generateur
             # puis pour chaque telechargement un recu.recup_ec()
-
+    """
 """
     bdd = BaseDDCl()
     bdd.test()
