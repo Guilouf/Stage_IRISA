02cf8f580208428689942d3b63d78541f7ced0cc
Guilouf <Guilouf@users.noreply.github.com>
2016-01-27 19:29:59 +0100

    La bbd marche putain!!!!!!

--- bddbis.py
+++ bddbis.py
@@ -22,8 +22,8 @@ class Accessions(Base):  # le truc (Base) c'est l'héritage
     Access = Column(String)
 
     #les relations
-    hasRefSeq = relationship("EC_numbers", secondary=association_table)
-    hasPrimaire = relationship("EC_numbers", secondary=association_table)
+    hasRefSeq = relationship("EC_numbers", secondary=association_table, back_populates="hasAcces")
+    # hasPrimaire = relationship("EC_numbers", secondary=association_table)
 
 
 class EC_numbers(Base):
@@ -32,6 +32,9 @@ class EC_numbers(Base):
 
     Id_ec = Column(Integer, primary_key=True)
     num_ec = Column(String)
+    hasAcces = relationship("Accessions", secondary=association_table, back_populates="hasRefSeq") # mouais, mais hasprimaire alors?
+    # backref ou back populate?,
+
 
 """
 class RefSeq(Base):  # bug avec le base ici
@@ -58,28 +61,44 @@ ses = Session()
 ####################################################################
 "Remplissages des tables"
 ####################################################################
-# ses.add(Accessions(Id=3, Access="grande bzacterie3"))
+# ses.add(Accessions(Id=0, Access="grande bzacterie0"))
+# ses.commit()
+#
+# ses.add(EC_numbers(Id_ec=1, num_ec="mechant num_ec1"))
 # ses.commit()
 
-# ses.add(EC_numbers(Id_ec=3, num_ec="mechant num_ec3"))
+# Relations: ok putain ca marche un peu..
+# deuximeEC = EC_numbers(Id_ec=13, num_ec="mechant num_ec13")
+# ses.add(Accessions(Id=4, hasRefSeq=[EC_numbers(Id_ec=12, num_ec="mechant num_ec11"),deuximeEC]))
 # ses.commit()
-
-# Relations:
-# Accessions.hasRefSeq(EC_numbers(Id_ec=3))
-
 ####################################################################
 "Requete sur les Tables "
 ####################################################################
 resulAcc = ses.query(Accessions).all()
 resulEc = ses.query(EC_numbers).all()
+# resuRela = ses.query()
 
 for laccessin in resulAcc:
-    print(laccessin.Id)
-    print(laccessin.Access)
+    # print(laccessin.Id)
+    # print(laccessin.Access)
+    print(laccessin.hasRefSeq)
+    pass
 
 for laccessinBis in resulEc:
-    print(laccessinBis.Id_ec)
-    print(laccessinBis.num_ec)
+    # print(laccessinBis.Id_ec)
+    # print(laccessinBis.num_ec)
+    listobjet = laccessinBis.hasAcces
+    print(listobjet)
+    for obj in listobjet:
+        print(obj.Id)
+    pass
+
+print(association_table.c.Accessions_tab_id)
+"""
+ok ca marche parfaitement, les [] montrent les objets qui n'ont pas de relations, pour les autres
+ca imprime entre [] les reférences des objets qu'il y a dans les relations. En plus c'est bien asymétrique
+Plus qu'a trouver le moyen de faire des updates..
+"""
 
 ####################################################################
 "Requete sur les relations "
