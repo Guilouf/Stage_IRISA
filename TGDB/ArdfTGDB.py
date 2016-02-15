8a8608ec79b086537d8f6ea58794c7a549acbe5f
Guilouf <Guilouf@users.noreply.github.com>
2016-02-15 22:55:49 +0100

    légère opti, passé de 2.5j de calcul à 30sec...

--- TGDB/ArdfTGDB.py
+++ TGDB/ArdfTGDB.py
@@ -7,23 +7,37 @@ from tinygraphdb import Tinygraphdb
 class TgdbToRDF:
     """
     J'ai l'impression que son truc me sort de l'aléatoire...
-    Du coup, est-ce idiot de parcourir le document à chaque noeud découvert?
-    Ben pas forcément vu que je n'ai pas acces directement au document, et que les noeuds et les relations sont séparées
-    En plus son truc est en aléatoire
+    Pour les assignement, apparement il peut y avoir un truc de type "assignment	X	score	Y", mais pas observé
+    dans le tgdb donc laissé tel quel pr l'instant.
     """
-    # TODO Du coup, est-ce idiot de parcourir le document à chaque noeud découvert? Oui et non, fait des générateurs!
+
     def __init__(self):
         self.tgdb = Tinygraphdb("tgdbRef.tgdb")
 
-        self.tgdb_nodes = (node for node in self.tgdb.getDicOfNode().values())  # générateur de noeuds
+        self.tgdb_nodes = (node for node in self.tgdb.getDicOfNode().values())  # générateur de noeuds TODO inutile ca en fait, et p e dangereux
         self.tgdb_rel = (rel for rel in self.tgdb.getTuplOfRelation())
 
-        self.fich_sortie = open("tgdbSortie.balec", 'w+')  # pas de close, mais balec
+        self.dico_rel = {}
 
-        self.fich_sortie.write("@prefix rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n\
-            @prefix tgdb:  <http://localhost:3030/essaiTGDB/tgdb> .\n\
-            @prefix metacyc: <http://localhost:3030/essaiTGDB/metacyc> .\n\
-            @prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#> .\n")
+        for rel in self.tgdb_rel:  # opti de ouf
+            idin = rel.getIdIn()
+            clefs = self.dico_rel.keys()
+            if idin in clefs:
+                self.dico_rel[rel.getIdIn()] += (rel,)
+            else:
+                self.dico_rel[rel.getIdIn()] = (rel,)
+
+
+        self.fich_sortie = open("tgdbSortie.balec", 'w+', encoding="utf-8")  # pas de close, mais balec
+
+        # le saut de ligne genere des tabs.. c normal crétin, tu prend les tabs de l'éditeur comme texte...
+        self.fich_sortie.write("# metacyc: les qualifiants directes des noeuds\n"\
+            "# tgdb: les entites(noeuds) et les relations\n" \
+            + "# rdf:type la classe metacyc du noeud(feuille); rdfs:subClassOf la superclasse de noeud\n" \
+            + "@prefix rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n" \
+            + "@prefix tgdb:  <http://localhost:3030/essaiTGDB/tgdb> .\n" \
+            + "@prefix metacyc: <http://localhost:3030/essaiTGDB/metacyc> .\n" \
+            + "@prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#> .\n")
 
     @staticmethod
     def rectif_stochio(dicoSto, idmetaparam):  # renvoit la liste d'identifiants bis à la place de la stochio
@@ -41,7 +55,7 @@ class TgdbToRDF:
 
         if stochio.isdigit():  # detecte si c un nombre
             return gener_ident_bis(int(stochio), idmetaparam)
-        elif stochio == 'n':
+        elif stochio == 'n': # TODO ca c 1, voir plus haut..
             return gener_ident_bis(1, idmetaparam)
         elif stochio == "n+1":
             return gener_ident_bis(2, idmetaparam)
@@ -49,28 +63,30 @@ class TgdbToRDF:
             return gener_ident_bis(2, idmetaparam)
         elif stochio == "2n":
             return gener_ident_bis(2, idmetaparam)
+        else: return gener_ident_bis(0, idmetaparam)  # TODO faire qq chose pour les n bizares...
 
     def nodes_to_rdf(self):
-        cpt_node = 0  # compteur de nombre de noeud parcouru, utilisé pour le flush
+        cpt_node = 0  # compteur de nombre de noeud parcouru, utilisé pour le flush TODO asuppr
         for node in self.tgdb_nodes:  # itere le générateur
             print("/!\ID du noeud:      ", node.getId())  # tgdb:"lid du truc"
             str_node = "tgdb:"+node.getId().lower()  # le sujet, cad le noeud
 
-            cpt_node += 1  # incre du compteur de noeud
+            cpt_node += 1  # incre du compteur de noeud TODO asuppr
             self.fich_sortie.write(str_node+" a metacyc:"+node.getClass()+" .\n")  # ecrit le nom du noeud et sa class
 
             # impression du misc
-            dicomisc = node.getMisc()  # TODO essayer de foutre ca en gener..
+            dicomisc = node.getMisc()
             for key in dicomisc.keys():  # /!\/!\/!\ les valeurs sont des listes.. META51128=>multiple
                 print("VALEUR DE CLE MULTIPLE") if len(dicomisc[key]) > 1 else None  # ternaire pr test multi val
                 for value in dicomisc[key]:  # les valeurs des clef sont des listes...
                     self.fich_sortie.write(str_node+"\t"+"metacyc:"+key.replace(" ", "")+'\t"'+value+'"'+" .\n")
 
-            relations_tpl = self.tgdb.getRelation(node.getId(), "in")  # renvoit un tuple des relations, none sinon.. TODO c'est ca qui plombe le truc.. getTuplOfRelation
+            relations_tpl = self.dico_rel.get(node.getId(), None)
+
             dico_pr_stochio = {}  # dico pr stocker les stochio associé au clés id
             if relations_tpl is not None:  # test si le noeud possède bien des relations
                 for rel in relations_tpl:  # parcourt les relations que possede le noeud
-                    # TODO rajouter les assignements
+
 
                     if rel.getType() == "is a":  # pour le cas des sous classes
                         self.fich_sortie.write(str_node+"\t"+"rdfs:subClassOf\t"+"tgdb:"+rel.getIdOut().lower()+" .\n")
@@ -78,19 +94,19 @@ class TgdbToRDF:
                     elif rel.getType() in ("produces", "consumes"):  # pour les stochios
                         # TODO pas oublier catalyse (en fait non, ya pas de stochio associé)
 
-                        valeur_sto_recti = TgdbToRDF.rectif_stochio(rel.getMisc(), rel.getIdOut())  # c une liste
-                        # TODO detecter que la liste ne soit pas un none...(heu?)
+                        valeur_sto_recti = TgdbToRDF.rectif_stochio(rel.getMisc(), rel.getIdOut())  # c une liste, parfois none qd erreur genre 4n
                         for ident_recti in valeur_sto_recti:
                             self.fich_sortie.write(str_node+"\t"+"tgdb:"+rel.getType()+" "+"tgdb:"+ident_recti+" .\n")
-                            pass
 
                         dico_pr_stochio[rel.getIdOut()] = valeur_sto_recti  # on charge dans le dico
-                        # TODO attention c plutot getidout() (ok mais verif)
 
-                        pass
                     else:  # pour les autres relations, hasname etc...
-                        self.fich_sortie.write(str_node+"\t"+"tgdb:"+rel.getType().replace(" ", "")+"\ttgdb:"+rel.getIdOut().lower()+" .\n")
-                    pass
+                        self.fich_sortie.write(str_node+"\t"+"tgdb:"+rel.getType().replace(" ", "")+"\ttgdb:"\
+                                               + rel.getIdOut().lower()+" .\n")
+                        if len(rel.getMisc()) != 0:  # pour ajouter les assignements (si yen a)
+                            assign = rel.getMisc()["assignment"]
+                            self.fich_sortie.write(str_node+"\t"+"tgdb:hasassign"\
+                                                   + "\ttgdb:"+assign[0].lower()+" .\n")
 
             # ecriture des ref la stochiometrie
             self.write_stochio(dico_pr_stochio)
@@ -99,32 +115,40 @@ class TgdbToRDF:
                 self.fich_sortie.flush()  # pour decharger le buffer avant le kbinterupt
                 print("flush!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
                 print(cpt_node)
-            # TODO enlever le flush une fois mis au point, quoique..
+            # TODO sert plus vraiment le flush
 
     def write_stochio(self, dico_pr_stochio_pr):
         for key in dico_pr_stochio_pr:
                 for ident_b in dico_pr_stochio_pr[key]:
-                    if len(dico_pr_stochio_pr[key]) > 1 :  # ca sert à rien d'écrire si l'ident pas modif..
+                    if len(dico_pr_stochio_pr[key]) > 1:  # ca sert à rien d'écrire si l'ident pas modif..
                         self.fich_sortie.write("tgdb:"+ident_b.lower()+" a\t"+"tgdb:"+key.lower()+" .\n")
 
-
-"""
-# alors en fait, en appel soit: self.write_misc(dicomiscparam) soit tgdb.write_misc(self, dicomiscparam)
-"""
-
-# inst = TgdbToRDF()
-# inst.nodes_to_rdf()
+inst = TgdbToRDF()
+inst.nodes_to_rdf()
 
 """
 tgdb = Tinygraphdb("tgdbRef.tgdb")
 
 tgdb_nodes = (node for node in tgdb.getDicOfNode().values())  # générateur de noeuds
-tgdb_rel = (rel for rel in tgdb.getTuplOfRelation())
+tgdb_rel = (rel for rel in tgdb.getTuplOfRelation())  # retourne un tuple d'objets. le get in est le truc a recupérer.
 
+dico_rel = {}
+
+for rel in tgdb_rel:
+    idin = rel.getIdIn()
+    clefs = dico_rel.keys()
+    if idin in clefs:
+        dico_rel[rel.getIdIn()] += (rel,)
+    else:
+        dico_rel[rel.getIdIn()] = (rel,)
+
+print("fini_dico_rel!!!")
+print(dico_rel["META2268"])
 
 # tgdb_rel()
 # for rel in tgdb_rel:
 #     print(rel.getIdOut())
+
 """
 ##################################################################
 # test pour la stochio
@@ -134,6 +158,7 @@ tgdb_rel = (rel for rel in tgdb.getTuplOfRelation())
 
 plusieurs possibilités de stochio: la classique, un chiffre. L'originale, un n. (META48933)
 l'extravagante, n+1 (META48942), la subversive, (n+1) (META52053), et la salope: 2n (META52053)(encore..)
+sournoise! |2n| !!!
 
 # pour retrouver les différentes clés
 
--- metacyc.py
+++
@@ -1,5 +0,0 @@
-"""
-Il faut travailler à partir des fichiers.dat, car les .col sont un sous ensemble de l'information
-le site:: http://bioinformatics.ai.sri.com/ptools/flatfile-format.html
-
-"""
