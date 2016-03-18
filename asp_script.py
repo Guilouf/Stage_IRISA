7fbe48deed258c20509767af7bec22e9e1359b58
guilouf <guigore@wanadoo.fr>
2016-03-18 16:53:36 +0100

    ecrit 10 lignes

--- asp_script.py
+++ asp_script.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python
 from pyasp.asp import *
 
+goptions = ''  # soluce max gringo
+soptions = ''  # solutions max solveur
+solver = Gringo4Clasp(gringo_options=goptions, clasp_options=soptions)
 
+# Liste des fichiers asp
+hidden = 'ASP/hidden.lp'
+base = 'ASP/metacyc_18.5.lp'
+query = 'ASP/explore.lp'
+# Solver
+result = solver.run([hidden, base, query], collapseTerms=True, collapseAtoms=False)
+
+for term in result[0]:
+    print(term)
