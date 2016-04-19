d7503de4bff5cde960dd2c82bc30347e1bc5ba2d
Guilouf <Guilouf@users.noreply.github.com>
2016-04-19 22:52:14 +0200

    continué de refaire les tableaux etc..

--- asp_script.py
+++ asp_script.py
@@ -54,6 +54,7 @@ result = solver.run([hidden, base, test, metagdb], collapseTerms=True, collapseA
 
 # impression de sortie ASP
 # for termm in result[0]:
+#     print(termm)
 #     print(termm.arguments)
 #     print(termm.predicate)
 
@@ -65,30 +66,82 @@ class Resultats:
 
     def __init__(self, m_result, m_list_acc):
         self.result = m_result[0]
-        self.list_access = m_list_acc
+        self.list_access = m_list_acc  # osef au final
         self.dico_vit = {}
+        self.list_ec_vit()
+        self.dico_souche = {}
+        self.bdd_temp()
 
     def list_ec_vit(self):
+        """
+        Associe une clef(la vitamine) à une valeur, les num ec qui sont contenus dans cette vitamines
+        Pour les ec des pathway de vitamine ( enzymeV(V,Ec) )
+        :return:
+        """
         for term in self.result:  # itère les termes
             if term.predicate == "enzymeV":  # ne retient que les terms enzymeV
-                # print(term)
-                # print(term.arguments[1])
                 if term.arguments[0] not in self.dico_vit:  # on ajoute les numéros ec associés à une clé de dico par vit
+                    # on est au niveau de la valeur de la clé vit
                     self.dico_vit[term.arguments[0]] = [term.arguments[1]]
                 else:
                     self.dico_vit[term.arguments[0]] += [term.arguments[1]]
         print(self.dico_vit)
 
+    def bdd_temp(self):
+        """
+        full_match(V,S,T,Ec)
+        Fait en gros une petite bdd temporaire
+        dico_souche: {'b9':{LEKW00000000.1: [[list_ec_full], [list_ec_part]] ,... }, ...}
+        """
+        for term in self.result:  # itère les termes
+            if term.predicate == "full_match":  # ne retient que les terms full_match !!
+                if term.arguments[0] not in self.dico_souche:  # ajout clé vitamine
+                    self.dico_souche[term.arguments[0]] = {}  # initialise la cle du dico
+                    if term.arguments[1] not in self.dico_souche[term.arguments[0]]:  # si ya pas la clef souche
+                        self.dico_souche[term.arguments[0]][term.arguments[1]] = [[term.arguments[3]], []]
+                        #                  vitamin              souche             num_ec_full        part_match
+                    else:  # si ya la cle de souche
+                        self.dico_souche[term.arguments[0]][term.arguments[1]][0] += [term.arguments[3]]
+                        #                                                      | pos full
 
-    def tab_effectifs(self):
-        pass
+                else:  # si ya la cle vitamine
+                    if term.arguments[1] not in self.dico_souche[term.arguments[0]]:  # si ya pas la clef souche
+                        self.dico_souche[term.arguments[0]][term.arguments[1]] = [[], [term.arguments[3]]]
+                        #                  vitamin              souche                  num_ec_part
+                    else:  # si ya la cle de souche
+                        self.dico_souche[term.arguments[0]][term.arguments[1]][1] += [term.arguments[3]]
 
 
+            if term.predicate == "rest_match":
+                # todo c'est ici crétin!!
+                pass
+        print(self.dico_souche)
 
-with open('exemple/ListeAccess', mode='r') as fichaccess:
+    def tab_comptage(self, vitamin=None):
+        if vitamin is None:
+            for key_vit in self.dico_souche.keys():  # itère les vitamines
+                for key_souche in self.dico_souche[key_vit].keys():
+                    # print(self.dico_souche[key_vit][key_souche])
+                    print(key_vit, key_souche)
+                    print("Nb_full", len(self.dico_souche[key_vit][key_souche][0]))
+                    print("Nb_rest", len(self.dico_souche[key_vit][key_souche][1]))
+
+with open('exemple/ListeAccess', mode='r') as fichaccess:  # ca aussi on sen fout
     listacc = [i.strip() for i in fichaccess]
     inst_resul = Resultats(result, listacc)
-    inst_resul.list_ec_vit()
+    inst_resul.tab_comptage()
+
+
+
+"""
+en gros, ce qu'il faut:
+
+{'b9':{LEKW00000000.1: [[list_ec_full], [list_ec_part]] ,... } }
+
+ou
+
+{'b9':[ [LEKW00000000.1 , [list_ec_full], [list_ec_part]] ,...] }
+"""
 
 
 """
